0fb47c3dd39364890fb150e164b6aab4876c28a5
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-07 00:29:16 +0600

    Add Overview in one single file

---
+++ Overview.cs
@@ -0,0 +1,127 @@
+﻿namespace StudentManager;
+
+
+public class Overview
+{
+    
+}
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentManager
+{
+
+    // -------------------
+    // Model
+    // -------------------
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+    }
+
+    // -------------------
+    // DbContext
+    // -------------------
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+
+    // -------------------
+    // Controller
+    // -------------------
+    public class StudentsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StudentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // READ (List)
+        public async Task<IActionResult> Index()
+        {
+            var students = await _context.Students.ToListAsync();
+            return View(students);
+        }
+
+        // READ (Single)
+        public async Task<IActionResult> Details(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            return View(student);
+        }
+
+        // CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Student student)
+        {
+            if (!ModelState.IsValid)
+                return View(student);
+
+            _context.Add(student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // UPDATE
+        public async Task<IActionResult> Edit(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student student)
+        {
+            if (!ModelState.IsValid)
+                return View(student);
+
+            _context.Update(student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // DELETE
+        public async Task<IActionResult> Delete(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
