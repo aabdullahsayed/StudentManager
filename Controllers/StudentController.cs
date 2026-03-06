8e67048c8490054801fc86004a4970dc45ed4ede
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-07 00:17:16 +0600

    Add Controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentManager.Data;
+using StudentManager.Models;
+public class StudentsController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public StudentsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // READ
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Students.ToListAsync());
+    }
+
+    // CREATE
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Student student)
+    {
+        _context.Add(student);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    // EDIT
+    public async Task<IActionResult> Edit(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        return View(student);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(Student student)
+    {
+        _context.Update(student);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    // DELETE
+    public async Task<IActionResult> Delete(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        return View(student);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        _context.Students.Remove(student);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+}
