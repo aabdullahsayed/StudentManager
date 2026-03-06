14bca5f6f1cfe6c1b2880bc11c3a7e7f42e9506f
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-07 00:16:43 +0600

    Add Model

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace StudentManager.Models;
+
+public class Student
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    
+    [Required]
+    public string Name { get; set; }
+    
+    [Required]
+    public int Age { get; set; }
+    
+    [EmailAddress]
+    public string Email { get; set; }
+}
